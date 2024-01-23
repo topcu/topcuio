a8ced1699cb672a1f939d9d349ec3d408ce69621
husnudarici <husnu.darici@glomil.com>
2024-01-23 23:13:34 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,45 +4,61 @@ import autoprefixer from 'gulp-autoprefixer';
 import cleanCSS from 'gulp-clean-css';
 import gulpSass from 'gulp-sass';
 import sassCompiler from 'sass';
+import uglify from 'gulp-uglify';
+import concat from 'gulp-concat';
+import htmlmin from 'gulp-htmlmin';
+import gulpIf from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
+import sassGlob from 'gulp-sass-glob';
+
 
 const sass = gulpSass(sassCompiler);
 
-// Static Server + watching scss/html files
+// Serve görevi
 gulp.task('serve', function () {
     browserSync.init({
         server: ".",
         notify: true
     });
 
-    gulp.watch("lib/css/*.scss", gulp.series('sass'));
+    gulp.watch("lib/css/**/*.{scss,css}", gulp.series('sass'));
     gulp.watch("*.html").on('change', browserSync.reload);
-    gulp.watch("lib/css/bootstrap/*.scss", gulp.series('bootstrap'));
 });
 
+// Sass görevi
 gulp.task('sass', function () {
-    return gulp.src('lib/css/*.scss') // main.scss dosyasını hedef alıyoruz
+    return gulp.src('lib/css/**/*.{scss,css}')
+        .pipe(sassGlob()) // SCSS dosyalarında glob kullanımını desteklemek için
+        .pipe(sourcemaps.init()) // Kaynak haritası oluştur
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 2 versions'],
         }))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('lib/css/'))
+        .pipe(sourcemaps.write('.')) // Kaynak haritasını yaz
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
-gulp.task('bootstrap', function () {
-    return gulp.src('lib/css/bootstrap/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 2 versions'],
-        }))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('lib/css/'))
+// JS birleştirme ve minify görevi
+gulp.task('js', function () {
+    return gulp.src('lib/js/**/*.js')
+        .pipe(concat('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
-gulp.task('default', gulp.parallel('sass', 'serve', 'bootstrap'));
+// Build görevi
+gulp.task('build', gulp.parallel('sass', 'js', function () {
+    return gulp.src(['index.html', 'page.html'])
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+}));
+
+// Default görevi
+gulp.task('default', gulp.series('sass', 'js', 'serve'));
